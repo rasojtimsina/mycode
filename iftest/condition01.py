02ec86d2591cc9950d193350cab0834971f6d2be
Rasoj Timsina <rasoj_timsina@yahoo.com>
2021-05-11 19:21:51 +0000

    if else commit message

---
+++ iftest/condition01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# create the string hostname.
+
+hostname = "MTG"
+
+# Test logic with the 'if' statement
+# what to do if this statement is not found to be true. 
+
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
---
+++ iftest/condition02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname?")
+
+## here we use the str.lower() method to return a lowercase string
+
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+elif hostname.lower() == "mtg":
+    print("hostname matches expected config")
+
+#exiting the script
+print("Exiting the script")
+
